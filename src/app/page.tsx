78213fd3e9a3844b432bd1964d3e509327d5b617
princiliz <ardechedemanou@gmail.com>
2025-03-24 16:48:49 +0100

    starter code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,40 @@
+"use client";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-15 gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-2 row-start-2 items-center w-full">
+        <div className="flex justify-center w-full">
+          <Image
+            className="dark:invert"
+            src="/image.png"
+            alt="Dockerlogo"
+            width={300}
+            height={300}
+            priority
+          />
+        </div>
+        <>
+          <p className="animated-text">Test Docker containerization</p>
+          
+          <style jsx>{`
+            .animated-text {
+              font-weight: bold;
+              font-size: 32px;
+              animation: blinkAndTransition 2s infinite;
+              text-align: center;
+              padding: 5px;
+            }
+            @keyframes blinkAndTransition {
+              0% { color: blue; opacity: 1; }
+              50% { color: white; opacity: 0.5; }
+              100% { color: blue; opacity: 1; }
+            }
+          `}</style>
+        </>
+        {/* <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
@@ -21,9 +43,9 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-        </ol>
+        </ol> */}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -47,10 +69,10 @@ export default function Home() {
           >
             Read our docs
           </a>
-        </div>
+        </div> */}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -94,7 +116,7 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
-        </a>
+        </a> */}
       </footer>
     </div>
   );
